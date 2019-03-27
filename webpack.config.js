2326967b61751bde1e9cdff482113847a117aabd
Kaiichiro Ota <kigh.ota@gmail.com>
2019-03-28 00:15:49 +0900

    ts-loader to babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,86 +2,103 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const path = require('path');
 const TS_MODULE = {
-    rules: [{
-        test: /\.tsx?$/,
-        use: 'ts-loader'
-    }],
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                // async/awaitに必要
+                targets: {
+                  node: 'current',
+                },
+              },
+            ],
+            '@babel/preset-typescript',
+            '@babel/preset-react',
+          ],
+        },
+      },
+    },
+  ],
 };
 
 module.exports = [
-    {
-        target: 'electron-main',
-        entry: './src/main/main.ts',
-        mode: 'development',
-        devtool: 'cheap-source-map',
-        output: {
-            path: path.join(__dirname, 'app', 'generated'),
-            filename: 'main.js'
-        },
-        module: TS_MODULE,
-        resolve: {
-            extensions: [
-                '.ts',
-                '.tsx',
-                '.js', // node_modulesのライブラリ読み込みに必要
-            ]
-        },
-        externals: [
-            function(context, request, callback) {
-                if (/^sqlite3$/.test(request)) {
-                    return callback(null, 'commonjs ' + request);
-                }
-                callback();
-            }
-        ],
-        plugins: [
-            new CleanWebpackPlugin(['./app/generated'])
-        ],
+  {
+    target: 'electron-main',
+    entry: './src/main/main.ts',
+    mode: 'development',
+    devtool: 'cheap-source-map',
+    output: {
+      path: path.join(__dirname, 'app', 'generated'),
+      filename: 'main.js',
     },
-    {
-        target: 'electron-renderer',
-        entry: './src/renderer/index.tsx',  // 起点となるファイル
-        // development は、 source map file を作成、再ビルド時間の短縮などの設定となる
-        // production は、コードの圧縮やモジュールの最適化が行われる設定となる
-        mode: 'development', // "production" | "development" | "none"
-        devtool: 'source-map',    // ソースマップのタイプ
-        // 出力先設定 __dirname は node でのカレントディレクトリのパスが格納される変数
-        output: {
-            path: path.join(__dirname, 'app', 'generated'),
-            filename: 'renderer.js'
-        },
-        module: TS_MODULE,
-        resolve: {
-            extensions: [
-                '.ts',
-                '.tsx',
-                '.js', // node_modulesのライブラリ読み込みに必要
-            ]
+    module: TS_MODULE,
+    resolve: {
+      extensions: [
+        '.ts',
+        '.tsx',
+        '.js', // node_modulesのライブラリ読み込みに必要
+      ],
+    },
+    externals: [
+      function(context, request, callback) {
+        if (/^sqlite3$/.test(request)) {
+          return callback(null, 'commonjs ' + request);
         }
+        callback();
+      },
+    ],
+    plugins: [new CleanWebpackPlugin(['./app/generated'])],
+  },
+  {
+    target: 'electron-renderer',
+    entry: './src/renderer/index.tsx', // 起点となるファイル
+    // development は、 source map file を作成、再ビルド時間の短縮などの設定となる
+    // production は、コードの圧縮やモジュールの最適化が行われる設定となる
+    mode: 'development', // "production" | "development" | "none"
+    devtool: 'source-map', // ソースマップのタイプ
+    // 出力先設定 __dirname は node でのカレントディレクトリのパスが格納される変数
+    output: {
+      path: path.join(__dirname, 'app', 'generated'),
+      filename: 'renderer.js',
     },
-    {
-        target: 'node',
-        entry: './src/cli/main.ts',
-        mode: 'development',
-        devtool: 'source-map',
-        output: {
-            path: __dirname,
-            filename: 'cli.js'
-        },
-        module: TS_MODULE,
-        resolve: {
-            extensions: [
-                '.ts',
-                '.js', // node_modulesのライブラリ読み込みに必要
-            ]
-        },
-        externals: [
-            function(context, request, callback) {
-                if (/^sqlite3$/.test(request)) {
-                    return callback(null, 'commonjs ' + request);
-                }
-                callback();
-            }
-        ]
-    }
+    module: TS_MODULE,
+    resolve: {
+      extensions: [
+        '.ts',
+        '.tsx',
+        '.js', // node_modulesのライブラリ読み込みに必要
+      ],
+    },
+  },
+  {
+    target: 'node',
+    entry: './src/cli/main.ts',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+      path: __dirname,
+      filename: 'cli.js',
+    },
+    module: TS_MODULE,
+    resolve: {
+      extensions: [
+        '.ts',
+        '.js', // node_modulesのライブラリ読み込みに必要
+      ],
+    },
+    externals: [
+      function(context, request, callback) {
+        if (/^sqlite3$/.test(request)) {
+          return callback(null, 'commonjs ' + request);
+        }
+        callback();
+      },
+    ],
+  },
 ];
